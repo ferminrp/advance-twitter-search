42c549938a78dbaed2dce6f99b699d3d8681ce1a
ferminrp <frodriguezpenelas@gmail.com>
2021-07-24 11:32:48 -0300

    fixed typo

--- src/App.svelte
+++ src/App.svelte
@@ -53,7 +53,7 @@
 
 <main>
 	<h1>Advance Twitter Search</h1>
-	<p class="description">Use this form to help you make more advance twitter searches easily.</p>
+	<p class="description">Use this form to help you make more advanced twitter searches easily.</p>
 
 	<TextInput bind:value={username} placeholder="elonmusk">Username</TextInput>
 	<TextInput bind:value={words} placeholder={group}>Search for:</TextInput>
